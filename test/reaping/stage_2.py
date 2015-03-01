acbb3e43912f6204fdccde291280aa4fb28311ff
Thomas Orozco <thomas@orozco.fr>
2015-03-01 11:01:49 -0500

    Randomize tests a little

--- test/reaping/stage_2.py
+++ test/reaping/stage_2.py
@@ -2,10 +2,11 @@
 from __future__ import print_function
 import subprocess
 import os
+import random
 
 
 if __name__ == "__main__":
     # Spawn lots of process
-    for i in range(0, 10):
-        cmd = ["sleep", str(1 + i % 2)]
+    for i in range(0, 100):
+        cmd = ["sleep", str(1 + i % 2 + random.random())]
         proc = subprocess.Popen(cmd)
