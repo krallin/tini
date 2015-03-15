84475c399bcd5e9631a1c0412b52ba0aa8c1a900
Thomas Orozco <thomas@orozco.fr>
2015-03-15 17:51:51 +0100

    Simplify build script
    Binaries are now pushed by Travis, we don't need to copy them anymore.

--- ddist.sh
+++ ddist.sh
@@ -5,20 +5,12 @@ set -o nounset
 REL_HERE=$(dirname "${BASH_SOURCE}")
 HERE=$(cd "${REL_HERE}"; pwd)
 
-DIST_DIR="${HERE}/dist"
-rm -rf "${DIST_DIR}"
-
 IMG="tini"
 NAME="${IMG}-dist"
-BIN="tini"
 
 docker build -t "${IMG}" .
 
-# Smoke test
-docker run -it --rm --entrypoint="/tini/${BIN}" "${IMG}" "-h" "--" "true"
-
-# Copy the binary and generated README
+# Copy the generated README
 docker run -it --entrypoint="/bin/true" --name="${NAME}" "${IMG}"
-docker cp "${NAME}:/tini/${BIN}" "${DIST_DIR}"
 docker cp "${NAME}:/tini/README.md" "${HERE}"
 docker rm "${NAME}"
