c8cc8274b838481b7418d61817344392a5e04a52
Thomas Orozco <thomas@orozco.fr>
2016-10-30 15:07:04 +0100

    ARM Cross Builds
    - Upgrade to Trusty for the ARM 64 toolchain.
    - Cross-compile to ARM.
    - Label binaries and packages when releasing.

---
+++ ci/install_deps.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+
+apt-get update
+
+apt-get install --no-install-recommends --yes \
+  build-essential git gdb valgrind cmake rpm \
+  python-dev libcap-dev python-pip python-virtualenv \
+  hardening-includes gnupg vim-common \
+  gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu libc6-dev-arm64-cross \
+  gcc-arm-linux-gnueabihf binutils-arm-linux-gnueabi libc6-dev-armhf-cross
+
+rm -rf /var/lib/apt/lists/*
